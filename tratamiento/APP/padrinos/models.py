ee62fc79ee7f41b8d57ff12510b854824b7e32a5
miguel.martinez <miguel.martinezq98@gmail.com>
2022-10-25 14:11:36 -0500

    .

--- tratamiento/APP/padrinos/models.py
+++ tratamiento/APP/padrinos/models.py
@@ -7,8 +7,35 @@ class padrinos (models.Model):
     nombre_padrino = models.CharField(max_length=50, blank=False,null=True, verbose_name='Nombre del padrino')
     apellido_padrino = models.CharField(max_length=50, blank=False,null=True, verbose_name='apellido del padrino')
     tipo_padrino = models.CharField(max_length=50, blank=False,null=True, verbose_name='tipo de padrino')
+    sexo = [
+        ('F', 'Femenino'),
+        ('M', 'Masculino'),
+        ('O', 'Otro')
+    ]
     sexo = models.CharField(max_length=50, blank=False,null=True, verbose_name='sexo del padrino')
     nivel_educativo = models.CharField(max_length=50, blank=False,null=True, verbose_name='nivel educativo del padrino')
     telefono = models.CharField(max_length=50, blank=False,null=True, verbose_name='telefono del padrino')
     direccion = models.CharField(max_length=50, blank=False,null=True, verbose_name='direccion del padrino')
     ciudad = models.CharField(max_length=50, blank=False,null=True, verbose_name='ciudad del padrino')
+    departamento = models.CharField(max_length=50, blank=False,null=True, verbose_name='departamento del padrino')
+    correo = models.CharField(max_length=50, blank=False,null=True, verbose_name='correo del padrino')
+    tiempo_apradinando = models.CharField(max_length=50, blank=False,null=True, verbose_name='tiempo apadrinado del padrino')
+    edad = models.PositiveIntegerField(default=0)
+    estrato = models.PositiveIntegerField(default=0)
+    create_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.apellido_padrino}  {self.nombre_padrino}'
+
+class Paciente_x_padrino(models.Model):
+    id_paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    id_padrino = models.ManyToManyField(padrinos)
+    create_at = models.DateField(auto_now_add=True)
+    
+    def obtener_padrino(self):
+        sueños = str([id_padrino for id_padrino in self.id_padrino.all().values_list(
+            'nombre', flat=True)]).replace("[", "").replace("]", "").replace("'", "")
+        return padrinos
+
+    def __str__(self):
+         return f'el padrino de {self.id_paciente} es  {self.obtener_padrino()}'
