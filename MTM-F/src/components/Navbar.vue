4169f2e1a142e4ddfbb6333a6da135eb81942fc1
Cristian David Hernandez Suarez <69637534+cristian-hernandez20@users.noreply.github.com>
2022-11-01 17:51:15 -0500

    d

--- MTM-F/src/components/Navbar.vue
+++ MTM-F/src/components/Navbar.vue
@@ -3,7 +3,7 @@
     <v-app-bar color="primary" dark app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase">
-        <span class="font-weight-light">SLH</span>
+        <span class="font-weight-light">Fundación</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-menu offset-y>
