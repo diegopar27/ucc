4376f78db7e4fc3506973e4da84eb4c0b2b071ea
danielmendez2 <daniel_20-09@hotmail.com>
2022-11-08 19:13:07 -0500

    login

--- MTM-F/src/views/Login.vue
+++ MTM-F/src/views/Login.vue
@@ -1,43 +1,40 @@
 <template>
-  <v-app id="inspire">
-    <v-content>
-      <v-container class="fill-height" fluid>
+  <v-app>
+    <v-container class="fill-height mx-auto" fluid>
+      <v-card class="elevation-12 mx-auto" width="500">
         <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="8" lg="7">
-            <v-card class="elevation-12">
-              <v-window v-model="step">
-                <v-window-item :value="1">
-                  <v-col cols="12" md="8" lg="12">
-                    <v-card-text class="mt-12">
-                      <h1 class="text-center display-2" style="color: #03a9f4">Iniciar sesión</h1>
-                      <br />
-                      <h4 class="text-center mt-4">Mujeres trabajando por el meta</h4>
-                      <v-form>
-                        <v-text-field label="Email" name="Email" prepend-icon="email" type="text" color="#03A9F4" v-model="usuario" />
-
-                        <v-text-field
-                          id="password"
-                          label="Password"
-                          name="password"
-                          prepend-icon="lock"
-                          type="password"
-                          color="#03A9F4"
-                          v-model="contraseña"
-                        />
-                      </v-form>
-                      <h3 class="text-center mt-4">Olvidaste tu contraseña ?</h3>
-                    </v-card-text>
-                    <div class="text-center mt-3">
-                      <v-btn rounded color="#03A9F4" dark v-on:click="login()">Iniciar sesión</v-btn>
-                    </div>
+          <v-col cols="12" sm="12" md="12" lg="12">
+            <h1 class="text-center primary--text">Iniciar sesión</h1>
+            <h4 class="text-center mb-2">Mujeres trabajando por el meta</h4>
+            <v-form ref="form" v-model="valid" lazy-validation id="form">
+              <v-card-text class="mt-12">
+                <v-row justify="center">
+                  <v-col cols="8">
+                    <v-text-field label="Email" required solo prepend-icon="email" type="email" color="#03A9F4" v-model="usuario" />
                   </v-col>
-                </v-window-item>
-              </v-window>
-            </v-card>
+                  <v-col cols="8">
+                    <v-text-field
+                      required
+                      id="password"
+                      label="Password"
+                      solo
+                      name="password"
+                      prepend-icon="lock"
+                      type="password"
+                      color="#03A9F4"
+                      v-model="contraseña"
+                    />
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-form>
+            <div class="text-center my-5">
+              <v-btn color="primary" :disabled="!valid" v-on:click="login()">Iniciar sesión</v-btn>
+            </div>
           </v-col>
         </v-row>
-      </v-container>
-    </v-content>
+      </v-card>
+    </v-container>
   </v-app>
 </template>
 
@@ -48,6 +45,7 @@ export default {
   data() {
     return {
       value1: "",
+      valid: false,
       value2: "",
       colorx: "#c72a75",
       colorx2: "#5252e8",
@@ -62,32 +60,19 @@ export default {
     }),
 
     async login() {
-      console.log("LOGIN");
+      console.log(this.$refs.form.validate());
+      this.$refs.form.validate();
       let data = {
         usuario: this.usuario,
         password: this.contraseña,
       };
-      console.log(data);
       let res = await this.LOGIN({ data });
       if (res) {
         sessionStorage.usuario = JSON.stringify(res);
         return this.$router.push("/");
       }
     },
-
-    // login() {
-    //   axios
-    //     .post("http://127.0.0.1:8000/api/user/lista", {
-    //       usuario: this.usuario,
-    //       contraseña: this.contraseña,
-    //     })
-    //     .then((resp) => console.log(resp))
-    //     .catch((err) => console.log(err));
-    // },
   },
-  // created() {
-  //   this.GETHISTORIAS();
-  // },
 };
 </script>
 
