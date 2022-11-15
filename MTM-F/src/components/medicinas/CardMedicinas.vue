71b58c5fe75034738b0a81556613aace78b0561d
danielmendez2 <daniel_20-09@hotmail.com>
2022-11-14 23:17:35 -0500

    .

---
+++ MTM-F/src/components/medicinas/CardMedicinas.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-dialog v-model="paciente.estado" max-width="500">
+    <v-card>
+      <v-footer color="primary" class="white--text">
+        <h1>Pacientes</h1>
+        <v-icon class="ml-2" color="white" size="35">mdi-book</v-icon>
+        <v-spacer> </v-spacer>
+        <v-btn icon @click="paciente.estado = false"> <v-icon color="white">mdi-exit-run</v-icon></v-btn>
+      </v-footer>
+      <v-container>
+        <v-row justify="start">
+          <v-col cols="3" class="my-0 py-1"><h3>Nombre medicamento</h3> </v-col>
+          <v-col cols="9" class="my-0 py-1">
+            <h7> {{ patologia.tipoPatolg }} </h7>
+          </v-col>
+          <v-col cols="3" class="my-0 py-1"><h3>Cantidad</h3> </v-col>
+          <v-col cols="9" class="my-0 py-1">
+            <h7>
+              {{ patologia.nombre }}
+            </h7>
+          </v-col>
+          <v-col cols="3" class="my-0 py-1"><h3>Formula médica</h3> </v-col>
+          <v-col cols="9" class="my-0 py-1">
+            <h7>
+              {{ patologia.tiempo }}
+            </h7>
+          </v-col>
+          <v-col cols="3" class="my-0 py-1"><h3>Condición</h3> </v-col>
+          <v-col cols="9" class="my-0 py-1">
+            <h7>
+              {{ patologia.condicion }}
+            </h7>
+          </v-col>
+          <v-col cols="3" class="my-0 py-1"><h3>Ciclo</h3> </v-col>
+          <v-col cols="9" class="my-0 py-1">
+            <h7>
+              {{ patologia.ciclo }}
+            </h7>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-divider class="mx-2 mt-2"></v-divider>
+      <v-footer color="white" class="white--text py-6">
+        <v-btn class="mx-auto"> Familiares </v-btn>
+        <v-btn class="mx-auto"> Padrinos </v-btn>
+      </v-footer>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    patologias: Object,
+  },
+};
+</script>
+
+<style></style>
---
+++ MTM-F/src/views/Patologias.vue
@@ -0,0 +1,141 @@
+<template>
+  <div>
+    <div>
+      <div>
+        <h1>Patologias</h1>
+        <div cols="12" md="4" lg="3">
+          <v-btn color="success" dark large> Crear patologia </v-btn>
+        </div>
+      </div>
+      <div>
+        <v-text-field cols="12" md="4" lg="3" v-model="search" label="Consultar por numero ID" class="purple-input" />
+        <template>
+          <v-data-table :search="search" :headers="headers" :items="pacientes" item-key="id" :items-per-page="5" class="elevation-1">
+            <template v-slot:[`item.actions`]="{ item }">
+              <v-icon small color="primary" class="mr-2" @click="verPaciente(item)"> mdi-book </v-icon>
+              <v-icon small color="warning" class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+              <v-icon small color="error" @click="deleteItem(item)"> mdi-delete </v-icon>
+            </template>
+          </v-data-table>
+        </template>
+      </div>
+    </div>
+    <PATOLOGIA :patologia="patologia" v-if="patologia.estado"></PATOLOGIA>
+  </div>
+</template>
+<script>
+import popapPaciente from "../components/pacientes/CardPacientes.vue";
+import axios from "axios";
+import { Global } from "../global";
+import { mapActions } from "vuex";
+
+export default {
+  components: {
+    PATOLOGIA: popapPaciente,
+  },
+  data: () => ({
+    search: "",
+    headers: [
+      {
+        text: "Tipo patologia",
+        align: "center",
+        sortable: false,
+        value: "nombre",
+      },
+      {
+        text: "Nombre",
+        align: "center",
+        sortable: false,
+        value: "apellido",
+      },
+      {
+        text: "Tiempo",
+        align: "center",
+        sortable: false,
+        value: "tipo_documento",
+      },
+      {
+        text: "Condición",
+        align: "center",
+        sortable: false,
+        value: "cedula",
+      },
+      {
+        text: "Ciclo",
+        align: "center",
+        sortable: false,
+        value: "cedula",
+      },
+      { text: "Actions", value: "actions", sortable: false },
+    ],
+    patologia: {
+      estado: false,
+      // nombre: "",
+      // apellidos: "",
+      // tipo_documento: "",
+      // cedula: "",
+      // edad: "",
+      // correo: "",
+      // eps: "",
+      // genero,
+    },
+    pacientes: [
+      {
+        nombre: "Cristian David",
+        apellido: "Hernandez Suarez",
+        tipo_documento: "C.C",
+        cedula: "1006689061",
+        edad: "21",
+        correo: "Cristian David",
+        eps: "Cajacopi",
+        genero: "Hombre",
+      },
+      {
+        nombre: "Cristian David",
+        apellido: "Hernandez Suarez",
+        tipo_documento: "C.C",
+        cedula: "1006689061",
+        edad: "21",
+        correo: "Cristian David",
+        eps: "Cajacopi",
+        genero: "Hombre",
+      },
+      {
+        nombre: "Cristian David",
+        apellido: "Hernandez Suarez",
+        tipo_documento: "C.C",
+        cedula: "1006689061",
+        edad: "21",
+        correo: "Cristian David",
+        eps: "Cajacopi",
+        genero: "Hombre",
+      },
+      {
+        nombre: "Cristian David",
+        apellido: "Hernandez Suarez",
+        tipo_documento: "C.C",
+        cedula: "1006689061",
+        edad: "21",
+        correo: "Cristian@18gmail.com",
+        eps: "Cajacopi",
+        genero: "Hombre",
+      },
+    ],
+  }),
+  async mounted() {},
+  methods: {
+    ...mapActions({}),
+    verPaciente(item) {
+      this.paciente.estado = true;
+      this.paciente.nombre = item.nombre;
+      this.paciente.apellidos = item.apellido;
+      this.paciente.tipo_documento = item.tipo_documento;
+      this.paciente.cedula = item.cedula;
+      this.paciente.edad = item.edad;
+      this.paciente.correo = item.correo;
+      this.paciente.eps = item.eps;
+      this.paciente.genero = item.genero;
+    },
+  },
+};
+</script>
