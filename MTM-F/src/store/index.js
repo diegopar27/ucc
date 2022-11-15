0baf39d04047e9ed657c3320e65abeaead369fb0
Cristian David Hernandez Suarez <69637534+cristian-hernandez20@users.noreply.github.com>
2022-11-14 23:49:19 -0500

    end

--- MTM-F/src/store/index.js
+++ MTM-F/src/store/index.js
@@ -5,6 +5,11 @@ Vue.use(Vuex);
 
 import padrinos from "./modules/padrinos";
 import usuario from "./modules/usuario";
+import examenes from "./modules/examenes";
+import medicinas from "./modules/medicinas";
+import pacientes from "./modules/pacientes";
+import tratamiento from "./modules/tratamiento";
+import patologias from "./modules/patologias";
 
 export default new Vuex.Store({
   state: {},
@@ -14,5 +19,10 @@ export default new Vuex.Store({
   modules: {
     usuario,
     padrinos,
+    examenes,
+    medicinas,
+    pacientes,
+    tratamiento,
+    patologias,
   },
 });
